5e7dcc43f603c19b3f11980f9396b037f0f8429d
piotr-winiarski <62816626+piotr-winiarski@users.noreply.github.com>
2022-01-04 19:00:11 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,3 +19,4 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
+game.showLongText("Witaj błędny rycerzu, klikaj przycisk jeślisz gotowy zdobyć górę ", DialogLayout.Bottom)
