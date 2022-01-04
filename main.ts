23608d822cbc88cf8d476010933603397f319130
piotr-winiarski <62816626+piotr-winiarski@users.noreply.github.com>
2022-01-04 16:43:04 +0100

    mapka

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+scene.setBackgroundColor(7)
+tiles.setTilemap(tilemap`level1`)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . f f e 2 f f f f f f 2 e f f . 
+    . f f f f f e e e e f f f f f . 
+    . . f e f b f 4 4 f b f e f . . 
+    . f f e 4 1 f d d f 1 4 e f . . 
+    f d f f e 4 d d d d 4 e f e . . 
+    f b f e f 2 2 2 2 e d d 4 e . . 
+    f b f 4 f 2 2 2 2 e d d e . . . 
+    f c f . f 4 4 5 5 f e e . . . . 
+    . f f . f f f f f f f . . . . . 
+    . . . . f f f . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
